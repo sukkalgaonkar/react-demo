53ff419d0e32575a498de1dfeeff803009ae128e
Sourabh U <sourabh.ukkalgaonkar@bacancy.com>
2024-01-15 17:11:20 +0530

    Added feature to add edit the player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,32 @@
-export default function Player( {name, symbol }) {
-    return (
-      <li>
-        <span className="player">
-          <span className="player-name">{name}</span>
-          <span className="player-symbol">{symbol}</span>
-        </span>
-        <button>Edit</button>
-      </li>
-    );
+import { useState } from "react";
+
+export default function Player( {initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = 'Edit';
+  
+  if (isEditing) {
+    editablePlayerName = <input type='text' required value={playerName} onChange={handleChange}/>
+    btnCaption = 'Save';
+  }
+
+  return (
+    <li>
+      <span className="player">
+        { editablePlayerName }
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick} >{btnCaption}</button>
+    </li>
+  );
 } 
