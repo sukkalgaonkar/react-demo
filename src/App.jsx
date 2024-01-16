72846972b7f489b4bcec39c2d1fb2aca87b2cf07
Sourabh U <sourabh.ukkalgaonkar@bacancy.com>
2024-01-16 15:23:28 +0530

    Modify Feature to update the winner loser logic

--- src/App.jsx
+++ src/App.jsx
@@ -22,16 +22,21 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    X: 'Player 1',
+    O: 'Player 2'
+  }); 
+
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
+    
     const { row, col } = square;
-
     gameBoard[row][col] = player; 
   }
 
@@ -47,7 +52,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol && 
       firstSquareSymbol == thirdSquareSymbol
     ) {
-        winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -66,15 +71,37 @@ function App() {
     })
   }
  
-  
+  function handleRestart() {
+    setGameTurns([]); 
+  }
+
+  function handlePlayerNameChange(symbol, newName){
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      }
+    });
+  }
+
   return <main>
     <div id="game-container">
       <ol id="players" className="highlight-player ">
-        <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'}/>
-        <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'}/>
+        <Player 
+          initialName='Player 1'
+          symbol='X'
+          isActive={activePlayer === 'X'}
+          onChangeName={handlePlayerNameChange}
+        />
+        <Player 
+          initialName='Player 2'
+          symbol='O'
+          isActive={activePlayer === 'O'}
+          onChangeName={handlePlayerNameChange}
+        />
       </ol>
 
-      { (winner || hasDraw) && <GameOver winner={winner}  /> }
+      { (winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} /> }
       <GameBoard onSelectSqure={handleSelectSquare}  board={gameBoard}/>
     </div>
 
